1724155cbea667c64411a86ff3a5d20991ae76a9
matej352 <matej.durin1@gmail.com>
2023-02-21 11:30:19 +0100

    add hello world page response

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
-console.log('Hello world')
+const express = require('express');
+const path = require('path');
+const port = 3000;
+
+
+const app = express();
+
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, "views"));
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+
+app.get('/', (req, res) => {
+
+    res.render('home', {});
+});
+
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`)
+  })
